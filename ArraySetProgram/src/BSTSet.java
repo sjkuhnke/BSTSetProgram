f0dce2476f1650c9dc98a0b9ce963472db62a964
shaej <shaej@10.193.200.19>
2023-05-19 00:47:17 -0500

    Implementation v. 1.0 on 5/19/23 at 00:47

---
+++ ArraySetProgram/src/BSTSet.java
@@ -0,0 +1,248 @@
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+import java.io.Serializable;
+
+public class BSTSet implements Set<String>, Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 4022434240979354040L;
+
+	// Nested Node class
+	private class Node implements Serializable {
+		/**
+		 * 
+		 */
+		private static final long serialVersionUID = -5451532909307792189L;
+		String data;
+		Node left, right;
+		
+		public Node(String data) {
+			this.data = data;
+		}
+	}
+	
+	// Fields
+	private Node root;
+	
+	public BSTSet() {	}
+	
+	@Override
+	public int size() {
+		return doSize(root);
+	}
+
+	private int doSize(Node n) {
+		if (n == null) return 0;
+		return doSize(n.left) + doSize(n.right) + 1;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return doSize(root) == 0;
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		if (!(o instanceof String)) {
+			return false;
+		}
+		String target = (String) o;
+		target = target.toLowerCase();
+		return doContains(root, target);
+	}
+	
+	private boolean doContains(Node n, String target) {
+		if (n == null) return false;
+		int c = target.compareTo(n.data);
+		
+		if (c == 0) {
+			return true;
+		} else if (c < 0) {
+			return doContains(n.left, target);
+		} else {
+			return doContains(n.right, target);
+		}
+	}
+
+	@Override
+	public Iterator<String> iterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public Object[] toArray() {
+		Object[] result = new Object[size()];
+		
+		doOrder(root, result, 0);
+		return result;
+	}
+
+	private int doOrder(Node n, Object[] r, int i) {
+		if (n == null) return i;
+		
+		i = doOrder(n.left, r, i);
+		r[i++] = n.data;
+		return doOrder(n.right, r, i);
+		
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public <T> T[] toArray(T[] a) {
+		if (a.length < size()) {
+			a = (T[]) java.lang.reflect.Array.newInstance(a.getClass().getComponentType(), size());
+		} else if (a.length > size()) {
+			a[size()] = null;
+		}
+		
+		doOrder(root, a, 0);
+	    return a;
+	}
+
+	@Override
+	public boolean add(String e) {
+		if (contains(e)) return false;
+		
+		e = e.toLowerCase();
+		Node elem = new Node(e);
+		return doAdd(root, elem);
+	}
+	
+	private boolean doAdd(Node n, Node e) {
+		if (n == null) {
+			root = e;
+			return true;
+		}
+		
+		int c = e.data.compareTo(n.data);
+		
+		if (c < 0) {
+			if (n.left == null) {
+				n.left = e;
+				return true;
+			} else {
+				return doAdd(n.left, e);
+			}
+		} else if (c > 0) {
+			if (n.right == null) {
+				n.right = e;
+				return true;
+			} else {
+				return doAdd(n.right, e);
+			}
+		}
+		return true;
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		if (!(o instanceof String)) {
+			return false;
+		}
+		
+		String e = (String) o;
+		e = e.toLowerCase();
+		if (!contains(e)) return false;
+		
+		Node target = new Node(e);
+		root = doRemove(root, target);
+		return true;
+	}
+	
+	private Node doRemove(Node n, Node e) {
+		if (n == null) return null;
+		
+		int c = e.data.compareTo(n.data);
+		
+		if (c < 0) {
+			n.left = doRemove(n.left, e);
+		} else if (c > 0) {
+			n.right = doRemove(n.right, e);
+		} else {
+			if (n.left == null) {
+				return n.right;
+			} else if (n.right == null) {
+				return n.left;
+			} else {
+				Node pre = findPre(n.left);
+				n.data = pre.data;
+				n.left = doRemove(n.left, pre);
+			}
+		}
+		
+		return n;
+	}
+	
+	private Node findPre(Node n) {
+		if (n == null) return n;
+		
+		return findPre(n.right);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends String> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public void clear() {
+		root = null;
+	}
+	
+	public String toString() {
+	    Object[] objectArray = toArray();
+	    String[] stringArray = new String[objectArray.length];
+
+	    for (int i = 0; i < objectArray.length; i++) {
+	        String s = (String) objectArray[i];
+	        stringArray[i] = toProperCase(s);
+	    }
+
+	    return String.join(", ", stringArray);
+	}
+	
+	public String toProperCase(String s) {
+	    if (s == null || s.isEmpty()) {
+	        return s;
+	    }
+
+	    StringBuilder sb = new StringBuilder();
+	    boolean capitalizeNext = true;
+
+	    for (char c : s.toCharArray()) {
+	        if (Character.isWhitespace(c)) {
+	            capitalizeNext = true;
+	            sb.append(c);
+	        } else {
+	            if (capitalizeNext) {
+	                sb.append(Character.toUpperCase(c));
+	                capitalizeNext = false;
+	            } else {
+	                sb.append(Character.toLowerCase(c));
+	            }
+	        }
+	    }
+	    
+	    return sb.toString();
+	}
+
+	
+}
---
+++ ArraySetProgram/src/Driver.java
@@ -0,0 +1,124 @@
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Driver implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6651871957967502739L;
+	private static final String FILE_PATH = "set.dat";
+	private BSTSet set;
+	
+	public static void main(String[] args) {
+        Driver driver = new Driver();
+        driver.loadSetFromFile();
+        driver.runUI();
+    }
+	
+	private void runUI() {
+		
+		JFrame frame = new JFrame("ArraySet UI");
+		JPanel panel = new JPanel();
+		JTextField tf = new JTextField(20);
+		JButton remove = new JButton("Remove");
+		JButton print = new JButton("Print");
+		JButton clear = new JButton("Clear");
+		remove.setBackground(Color.YELLOW);
+		print.setBackground(Color.GREEN);
+		clear.setBackground(Color.RED);
+		
+		panel.add(tf);
+		panel.add(remove);
+		panel.add(print);
+		panel.add(clear);
+		
+		tf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String text = set.toProperCase(tf.getText());
+				if (set.add(text)) {
+                	System.out.println("Added " + text);
+                } else {
+                	System.out.println(text + " already in set");
+                }
+				tf.selectAll();
+			}
+		});
+		
+		remove.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String text = set.toProperCase(tf.getText());
+				if (set.remove(text)) {
+					System.out.println("Removed " + text);
+				} else {
+					System.out.println(text + " not present in set");
+				}
+			}
+		});
+		
+		print.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(set.toString());
+            }
+        });
+		
+		clear.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	int option = JOptionPane.showConfirmDialog(frame, "Are you sure you want to clear the set?", "Confirm Clear", JOptionPane.YES_NO_OPTION);
+                if (option == JOptionPane.YES_OPTION) {
+                    set.clear();
+                    System.out.println("\nSet cleared.\n");
+                }
+            }
+        });
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(panel);
+        frame.pack();
+        frame.setVisible(true);
+        
+        frame.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+                saveSetToFile(set);
+            }
+        });
+	}
+	
+	private static void saveSetToFile(BSTSet set) {
+        try (FileOutputStream fileOut = new FileOutputStream(FILE_PATH);
+             ObjectOutputStream objectOut = new ObjectOutputStream(fileOut)) {
+            objectOut.writeObject(set);
+            System.out.println("Set saved to " + FILE_PATH);
+        } catch (IOException ex) {
+            System.out.println("Error saving set to file: " + ex.getMessage());
+        }
+    }
+	
+	private void loadSetFromFile() {
+        try (FileInputStream fileIn = new FileInputStream(FILE_PATH);
+             ObjectInputStream objectIn = new ObjectInputStream(fileIn)) {
+            set = (BSTSet) objectIn.readObject();
+            System.out.println("Set loaded from set.dat successfully.");
+        } catch (IOException | ClassNotFoundException ex) {
+            System.out.println("Error loading from set.dat: " + ex.getMessage());
+            set = new BSTSet();
+        }
+    }
+}
